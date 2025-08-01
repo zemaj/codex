ff019b3a6b42de020c7e6c4efe404b0c6df4844c
Michael Bolin <mbolin@openai.com>
2025-08-01 14:08:36 -0700

    chore: introduce SandboxPolicy::WorkspaceWrite::use_exact_writable_roots
    Without this change, it is challenging to create integration tests to
    verify that the folders not included in `writable_roots` in
    `SandboxPolicy::WorkspaceWrite` are read-only because, by default,
    `get_writable_roots_with_cwd()` includes `TMPDIR`, which is where most integrationt
    tests do their work.
    
    This introduces a `use_exact_writable_roots` option to disable the default
    includes returned by `get_writable_roots_with_cwd()`.

--- codex-rs/core/src/config.rs
+++ codex-rs/core/src/config.rs
@@ -350,6 +350,7 @@ impl ConfigToml {
                 Some(s) => SandboxPolicy::WorkspaceWrite {
                     writable_roots: s.writable_roots.clone(),
                     network_access: s.network_access,
+                    include_default_writable_roots: true,
                 },
                 None => SandboxPolicy::new_workspace_write_policy(),
             },
@@ -720,6 +721,7 @@ writable_roots = [
             SandboxPolicy::WorkspaceWrite {
                 writable_roots: vec![PathBuf::from("/tmp")],
                 network_access: false,
+                include_default_writable_roots: true,
             },
             sandbox_workspace_write_cfg.derive_sandbox_policy(sandbox_mode_override)
         );
