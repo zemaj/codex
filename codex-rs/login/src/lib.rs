7c1871e2dffac4a6416b6fe963269877e9ee263e
Michael Bolin <mbolin@openai.com>
2025-08-07 14:19:41 -0700

    chore: rename CodexAuth::new() to create_dummy_codex_auth_for_testing() because it is not for general consumption

--- codex-rs/login/src/lib.rs
+++ codex-rs/login/src/lib.rs
@@ -51,21 +51,6 @@ impl PartialEq for CodexAuth {
 }
 
 impl CodexAuth {
-    pub fn new(
-        api_key: Option<String>,
-        mode: AuthMode,
-        auth_file: PathBuf,
-        auth_dot_json: Option<AuthDotJson>,
-    ) -> Self {
-        let auth_dot_json = Arc::new(Mutex::new(auth_dot_json));
-        Self {
-            api_key,
-            mode,
-            auth_file,
-            auth_dot_json,
-        }
-    }
-
     pub fn from_api_key(api_key: String) -> Self {
         Self {
             api_key: Some(api_key),
@@ -140,6 +125,17 @@ impl CodexAuth {
 
                 token_data.account_id.clone()
             }
+        }
+    }
+
+    /// Consider this private to integration tests.
+    pub fn create_dummy_chatgpt_auth_for_testing(auth_dot_json: AuthDotJson) -> Self {
+        let auth_dot_json = Arc::new(Mutex::new(Some(auth_dot_json)));
+        Self {
+            api_key: None,
+            mode: AuthMode::ChatGPT,
+            auth_file: PathBuf::new(),
+            auth_dot_json,
         }
     }
 }
